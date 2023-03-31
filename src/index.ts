6ffe056a5b5fd79669a447dac031264ceb929330
mkabatek <mkabatek@gmail.com>
2023-03-31 09:26:40 -0600

    Initial commit of boilerplate code

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+const world = 'world';
+
+export function hello(who: string = world): string {
+  return `Hello ${who}! `;
+}
+
+function main() {
+  console.log(hello(process.argv[2]));
+}
+
+main();
